22caa5fd57b2302c00845e59d3ee103904ed3f29
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-28 17:49:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+  const productId = req.params.productId;
+
+  res.status(200).json({
+    productId: productId
+  });
 });
 
 module.exports = app;
